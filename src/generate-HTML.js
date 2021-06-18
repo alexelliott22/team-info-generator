86cb6018d95545675e57db11ab7043835a086451
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-18 15:49:01 -0400

    write html for new file

---
+++ src/generate-HTML.js
@@ -0,0 +1,117 @@
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
+
+
+
+const positions = [
+    {
+        name: 'alex',
+        id: '3',
+        email: 'a@mail.com',
+        officeNum: '5',
+        teamMemberAdded: 'Intern'
+      },
+      {
+        name: 'b',
+        id: '5',
+        email: 'b@mail.com',
+        school: 'harding',
+        teamMemberAdded: 'Engineer'
+      },
+      {
+        name: 'joe smith',
+        id: '78',
+        email: 'js@mail.com',
+        github: 'jsmith',
+        teamMemberAdded: "I don't want to add any more team members"
+      }
+];
+
+function determinePosition(teamMember) {
+    if(teamMember.github) {
+        let engineer = new Engineer(teamMember.name, teamMember.id, teamMember.email, teamMember.github)
+        
+        createTeamMemberCards(engineer);
+    } else if(teamMember.school) {
+        let intern = new Intern(teamMember.name, teamMember.id, teamMember.email, teamMember.school)
+
+        createTeamMemberCards(intern);
+    } else {
+        let manager = new Manager(teamMember.name, teamMember.id, teamMember.email, teamMember.officeNum)
+
+        createTeamMemberCards(manager);
+    }
+}
+
+function getLastQuality(teamMember) {
+    if(teamMember.getRole() == 'Manager') {
+        return `<p class="card-text">Office Number: ${teamMember.getOfficeNumber()}</p>`
+    } else if(teamMember.getRole() == 'Engineer') {
+        return `<p class="card-text">Github: <a href="https://github.com/${teamMember.getGithub()}">${teamMember.getGithub()}</a></p>`
+    } else {
+        return `<p class="card-text">University: ${teamMember.getSchool()}</p>`
+    }
+}
+
+function createTeamMemberCards(teamMember) {
+
+    
+    return `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${teamMember.name}</h5>
+            <h6 class="card-subtitle mb-2">${teamMember.getRole()}</h6>
+            <p class="card-text">ID: ${teamMember.getId()}</p>
+            <p class="card-text">Email: <a href="mailto:${teamMember.getEmail()}">${teamMember.getEmail()}</a></p>
+            ${getLastQuality(teamMember)}
+        </div>
+  </div>
+    `
+}
+
+
+function lookThroughArray(positions) {
+    positions.forEach(teamMember => {
+        determinePosition(teamMember);
+    });
+}
+
+
+
+function generateHTML(positions) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <title>My Team</title>
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+        <div class="wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 col-lg-4">
+                ${lookThroughArray(positions)}
+              </div>
+            </div>
+          </div>
+        </div>
+    </body>
+    </html>
+    `
+}
+
+generateHTML(positions);
+
+
+
+
+// module.exports = generateHTML;
--- src/template-maker.js
+++
@@ -1,3 +0,0 @@
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
